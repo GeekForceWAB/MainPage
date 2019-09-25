b62973b000b64d201582f62638053e142816d5a1
Timo <31155789+D3CRYqT@users.noreply.github.com>
2019-09-25 10:15:01 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var red = [359, 100, 29.8]; //HSL color code
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var bluefield = [199, 100, 22.9];
+var austin = [126, 24, 65];
+
+var myName = "geekforce.club";
+var letterColors = [red, orange, green, blue, purple,bluefield, austin];
+var bubbleShape = "square";
+
+drawName(myName, letterColors);
+bounceName();
+bounceBubbles();
